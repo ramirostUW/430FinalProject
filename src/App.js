3fa12ba609d5c71c7aefb8acfe52e3ee502bb39e
Ramiro Steinmann Petrasso <ramirost@live.com>
2021-05-30 19:05:46 -0400

    Adding react stuff

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Info 430 Final Project</h1>
+      <h2>By: Michael, Olivia, Pranav. and Ramiro</h2>
+    </div>
+  );
+};
+
+export default App;
